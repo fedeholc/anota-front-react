e545dfee2ac6445a66c9e6b98743df747d8e0e42
fedeholc <federicoholc@gmail.com>
2023-05-30 17:23:10 -0300

    remove unsed code

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,22 @@
 import { SetNotesFilterContext } from "../NotesContext";
 import { useContext } from "react";
-import { SearchOutlined } from "@ant-design/icons";
 import "./SearchBar.css";
-import { Input  } from "antd";
-
+import { Input } from "antd";
 
 export default function SearchBar() {
   const { Search } = Input;
 
   const setNotesFilter = useContext(SetNotesFilterContext);
 
-  //TODO: falta icono y funcion de clear search
-
   function handleChange(e) {
-    setNotesFilter(prev => {
-      return ({...prev,
-      text: e.target.value});
+    setNotesFilter((prev) => {
+      return { ...prev, text: e.target.value };
     });
   }
 
   return (
-    <div
-    
-    >
-    {/*   <input
-        className="search__input"
-        type="text"
-        placeholder="Buscar..."
-        onChange={handleChange}
-      /> */}
-{/*       <SearchOutlined className="search__icon" />
- */}      <Search
+    <div>
+      <Search
         placeholder="Buscar..."
         allowClear
         onChange={handleChange}
