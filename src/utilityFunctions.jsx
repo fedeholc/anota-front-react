e87e3efcd316e50a8527b34f1301608eaba59789
fedeholc <federicoholc@gmail.com>
2023-06-02 18:22:24 -0300

    clean code

--- src/utilityFunctions.jsx
+++ src/utilityFunctions.jsx
@@ -1,5 +1,3 @@
-import { useState, useLayoutEffect } from "react";
-
 // función que toma un string de tags separados por coma y devuelve un array de tags, sin espacios al principio ni al final, y sin tags vacíos
 export function getTagsArray(tagsString) {
   const tagsArray = tagsString.split(",");
@@ -36,32 +34,3 @@ export function getFormattedDateTime() {
 export function dateTimeJStoDB(dateTime) {
   return dateTime.slice(0, 10) + " " + dateTime.slice(11, 19);
 }
-
-//! TODO: revisar si lo estoy usando y para qué
-export const useIsOverflow = (ref, callback) => {
-  const [isOverflow, setIsOverflow] = useState();
-
-  useLayoutEffect(() => {
-    const { current } = ref;
-
-    const trigger = () => {
-      // eslint-disable-next-line no-unused-vars
-      const hasOverflow = current.scrollHeight > current.clientHeight;
-      const divs = current.querySelectorAll(".note__body");
-      const miarr = Array.from(divs);
-      //console.log("divs:", divs);
-      //console.log("miarr", miarr);
-      let fede = miarr.map((e) => e.scrollHeight > e.clientHeight);
-      //console.log("map", fede);
-      /*       setIsOverflow(hasOverflow);
-       */ setIsOverflow("fede");
-      if (callback) callback(fede);
-    };
-
-    if (current) {
-      trigger();
-    }
-  }, [callback, ref]);
-
-  return isOverflow;
-};
