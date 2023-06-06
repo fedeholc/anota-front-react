161a688fd6fee9af40d11fa2b0f7c2c961ab853d
fedeholc <federicoholc@gmail.com>
2023-06-06 19:01:09 -0300

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <>
       <div className="app__container">
         <div className="app__sticky-navbar">
-          ANOTA (Another Note Taking App)
+          📓ANOTA (Another Note Taking App)
         </div>
         <div className="app__main-grid">
           <div>
