308adcef8fed801d505583bd7766aa9bec43081b
fedeholc <federicoholc@gmail.com>
2023-08-13 17:59:50 -0300

    fix warning

--- src/components/toolbar-buttons/LayoutButton.jsx
+++ src/components/toolbar-buttons/LayoutButton.jsx
@@ -3,29 +3,38 @@ import { NotesLayoutContext, SetNotesLayoutContext } from "../../NotesContext";
 import { useContext } from "react";
 import { ReactComponent as LayoutMasonryIcon } from "../../assets/LayoutMasonryIcon.svg";
 import { ReactComponent as LayoutListIcon } from "../../assets/LayoutListIcon.svg";
+import { RiLayoutMasonryLine, RiFileListLine } from "react-icons/ri";
 import { Tooltip } from "antd";
 
 export default function LayoutButton() {
   const SetNotesLayout = useContext(SetNotesLayoutContext);
   const notesLayout = useContext(NotesLayoutContext);
 
+  function handleLayoutChange() {
+    SetNotesLayout((prev) => (prev === 0 ? 1 : 0));
+  }
+
   return (
     <div>
       {notesLayout === 0 && (
         <Tooltip placement="topLeft" title="vista Lista">
-          <LayoutListIcon
-            onClick={() => SetNotesLayout((prev) => (prev === 0 ? 1 : 0))}
-            className="toolbar__icon"
-          ></LayoutListIcon>
+          <div>
+            <LayoutListIcon
+              onClick={handleLayoutChange}
+              className="toolbar__icon"
+            ></LayoutListIcon>
+          </div>
         </Tooltip>
       )}
 
       {notesLayout === 1 && (
         <Tooltip placement="topLeft" title="vista Mosaico">
-          <LayoutMasonryIcon
-            onClick={() => SetNotesLayout((prev) => (prev === 0 ? 1 : 0))}
-            className="toolbar__icon"
-          ></LayoutMasonryIcon>
+          <div>
+            <LayoutMasonryIcon
+              onClick={() => SetNotesLayout((prev) => (prev === 0 ? 1 : 0))}
+              className="toolbar__icon"
+            ></LayoutMasonryIcon>
+          </div>
         </Tooltip>
       )}
     </div>
