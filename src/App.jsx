64d25a5d70825eea7040f5258b9310c24432ed98
fedeholc <federicoholc@gmail.com>
2024-02-25 14:48:52 -0300

    Refactor code: Remove commented out code and add margin to login module

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,7 @@ import { supabase } from "./supabaseClient";
 import { Login } from "./components/login/login";
 import { LoginAuth } from "./components/login/loginAuth";
 
-/* const supabase = createClient(
-  "https://xbnjcziobgswkczsvssv.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhibmpjemlvYmdzd2tjenN2c3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyNDc1MzAsImV4cCI6MjAwODgyMzUzMH0.FEYcuWPO4B4kDmOMbmXqy_K6TsW8xoRAF9CQCo0SRUU"
-); */
-
 function App() {
-  /* const [supabaseClient, setSupabaseClient] = useState(
-    useContext(supabaseClientContext)
-  ); */
-
-  console.log("sc", supabase);
-
   const [isCollapsed, setIsCollapsed] = useState(
     JSON.parse(localStorage.getItem("isCollapsed") || false)
   );
