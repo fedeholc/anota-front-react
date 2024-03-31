15c24e93235f6c67faf466798d0d96b579160a81
fedeholc <federicoholc@gmail.com>
2024-03-31 19:29:04 -0300

    fix vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
   },
-
-})
+});
