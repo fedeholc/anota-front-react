e37841327f87239ab7abe4019a6305b56c225214
fedeholc <federicoholc@gmail.com>
2024-03-31 19:51:14 -0300

    Add @vitest/ui dependency and update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,7 +6,11 @@ import App from "./App";
 describe("App", () => {
   it("renders headline", () => {
     render(<App />);
-
+    screen.debug();
     expect(screen.getByText(/anota/i)).toBeInTheDocument();
+  });
+  it("title snapshot", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
 });
